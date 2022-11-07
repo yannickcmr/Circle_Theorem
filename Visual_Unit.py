08434f1be01d27e80b68411fa41c3712b8f29372
Yannick <y.ciomer@web.de>
2022-11-07 23:27:08 +0100

    fixed bugs

--- Visual_Unit.py
+++ Visual_Unit.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from Calc_Unit import Circle_Theorem, Randomize_Matrix
 
 """ Visualization """
@@ -8,40 +9,46 @@ def Split_Values(array: list):
     y_values = [x[1] for x in array]
     return x_values, y_values
 
-def Get_Scale(array):
+def Get_Scale_x(array):
     range_max = [x[0] + x[1] for x in array]
     range_min = [x[0] - x[1] for x in array]
-    return [min(range_min), max(range_max)]
+    return [min(range_min) - 1, max(range_max) + 1]
+
+def Get_Scale_y(array):
+    range_max = max(array ,key=lambda item:item[1])[1]
+    return [-range_max - 1, range_max + 1]
+
 
 def Visualize_Circle_Theorem(circle_values: list, exact_values: list = []):
     colours = ["blue", "green", "yellow", "orange", "magenta", "brown"]
     
-    x_val, y_val = Split_Values(circle_values)
+    x_val, rad_val = Split_Values(circle_values)
     figure, axes = plt.subplots()
 
     for i in range(0, len(x_val)):
-        axes.add_artist(plt.Circle((x_val[i], 0), y_val[i], alpha=0.25, color=colours[i]))
+        axes.add_artist(plt.Circle((x_val[i], 0), rad_val[i], alpha=0.25, color=colours[i]))
         axes.add_artist(plt.Circle((x_val[i], 0), 0.02, alpha=0.69, color=colours[i]))
-
-    if len(exact_values) > 0:
-        x_exact, y_exact = Split_Values(exact_values)
-        plt.scatter(x_exact, y_exact , color="red")
 
     axes.set_aspect("equal")
     plt.title("Kreisapproximation der Eingabe")
     plt.grid(True, which="both")
 
-    plt.xlim(Get_Scale(circle_values))
-    plt.ylim(Get_Scale(circle_values))
+    plt.xlim(Get_Scale_x(circle_values))
+    plt.ylim(Get_Scale_y(circle_values))
 
     plt.axhline(linewidth=2, color="black")
     plt.axvline(linewidth=2, color="black")
+
+    if len(exact_values) > 0:
+        x_exact, y_exact = Split_Values(exact_values)
+        plt.scatter(x_exact, y_exact , color="red", s=25, marker="*", zorder=2)
+
     plt.show()
 
 if __name__ == "__main__":
-    test_matrix = Randomize_Matrix(2, 2)
+    test_matrix = Randomize_Matrix(15, 2)
     print(test_matrix)
     theorem, exact = Circle_Theorem(test_matrix)
-    print(theorem)
+    print(f"{theorem}\n{exact}")
     Visualize_Circle_Theorem(theorem, exact)
 
