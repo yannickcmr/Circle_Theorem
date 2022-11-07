39f3221ddeaca127a38f32028e5af6acf4a33e71
Yannick <y.ciomer@web.de>
2022-11-07 23:41:08 +0100

    added clarifications

--- Calc_Unit.py
+++ Calc_Unit.py
@@ -2,24 +2,30 @@ import numpy as np
 
 """ Calculation """
 
+# Create a random matrix, mainly for testing.
 def Randomize_Matrix(val_range: int, size: int) -> np.ndarray:
     return np.random.randint(-val_range, val_range, size=(size, size))
 
+# If the input matrix is not a ndarray, it will be converted.
 def Format_Matrix(matrix) -> np.ndarray:
     matrix = np.array((matrix), dtype=float)
-    shape_matrix = matrix.shape
+    shape_matrix = matrix.shape # (size x, size y)
+
     if shape_matrix[0] != shape_matrix[1]:
         raise Exception (f"Input matrix is not square: {shape_matrix}")
     return matrix
 
+# split np.complex into rounded tuple: (real part, img part) 
 def Round_Complex(value: np.complex128) -> tuple:
     return (np.around(value.real, decimals=3), np.around(value.imag, decimals=3))
 
+# Calculate exact eigenvalues.
 def Exact_Eigenvalues(matrix: np.ndarray) -> np.ndarray:
     eigenvalues = np.linalg.eig(matrix)[0]
     return [Round_Complex(x) for x in eigenvalues]
 
-def Gerschgorin(matrix: np.ndarray) -> np.ndarray:
+# Implimentation of the circle theorem. Returns list of tuple consisting of tuple of center and radii: [(center, radius),...] 
+def Gerschgorin(matrix: np.ndarray) -> list:
     eigenvalues = []
     for i in range(len(matrix)):
         sum_row = np.sum(np.abs(matrix[i])) - np.abs(matrix[i][i])
